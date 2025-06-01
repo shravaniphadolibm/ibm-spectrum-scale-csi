a8da81e89f6cc4b2c870f4975d67391a61c4e378
shravani <shravani.phadol@ibm.com>
2025-06-01 22:39:39 +0530

    solving  the G issue by calculating G into decimal bytes
    Signed-off-by: shravani <shravani.phadol@ibm.com>

--- driver/csiplugin/gpfs_util.go
+++ driver/csiplugin/gpfs_util.go
@@ -577,7 +577,8 @@ func ConvertToBytes(inputStr string) (uint64, error) {
 	case "m", "mb", "megabytes", "megabyte":
 		retValue *= (1024 * 1024)
 	case "g", "gb", "gigabytes", "gigabyte":
-		retValue *= (1024 * 1024 * 1024)
+		//retValue *= (1024 * 1024 * 1024)
+		retValue *= 1000000000
 	case "t", "tb", "terabytes", "terabyte":
 		retValue *= (1024 * 1024 * 1024 * 1024)
 	default:
